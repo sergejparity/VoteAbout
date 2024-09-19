c823663f2b4cd025d20bf375b6f5bdeaeceece96
Tumusiime Francis <80533694+tumutech@users.noreply.github.com>
2024-09-19 21:47:14 +0300

    10 frontend design results page (#13)
    * frontend-design-results-page
    
    * results-page

--- frontend/src/app/results/page.tsx
+++ frontend/src/app/results/page.tsx
@@ -1,12 +1,12 @@
 'use client';
-import NavBar from '../../components/NavBar'; 
+import NavBar from "@/components/NavBar";
+import ResultsPage from "@/components/Results";
 
 const CreatePoll: React.FC = () => {
   return (
     <div className="h-screen flex flex-col top">
         <NavBar />
-      <h1 className="text-4xl font-bold">Results</h1>
-      <p className="text-lg mt-5">View Results for voting.</p>
+      <ResultsPage />
     </div>
   );
 };
